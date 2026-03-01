b94e1169ef39b681e100741e7d4ce04fd7d9bd70
MaanPatel2005 <maanvpatel@gmail.com>
2026-02-28 18:22:05 -0800

    Added nicer UI for Tools page

--- aom-ui/src/App.jsx
+++ aom-ui/src/App.jsx
@@ -1,10 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import HomePage from './HomePage.jsx';
 import AomUploadPage from './AomUploadPage.jsx';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/"        element={<HomePage />} />
         <Route path="/toolkit" element={<AomUploadPage />} />
